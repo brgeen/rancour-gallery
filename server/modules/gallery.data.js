7b5a9d7e42b7f4cbd423b528bea7537f699c0171
Brooks Geenen <brgeen@gmail.com>
2019-10-25 16:06:09 -0500

    populate gallery with images and map them to the DOM

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat.jpg', description: 'Goat in Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/cat.jpg', description: 'This is my cat Tabby.', likes: 3 },
+    { id: 3, path: 'images/chipmunk.jpg', description: 'I found this chipmunk in the park.', likes: 2 },
+    { id: 4, path: 'images/dog.jpg', description: 'My friends dog, Howard', likes: 5 },
+    { id: 5, path: 'images/weasel.jpg', description: 'Weasels are cute looking', likes: 15 }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    this.galleryList();
+  }
+
+  state = {
+    galleryList: [],
+  };
+
+  galleryList() { // we do this outside of render becuase of lifecycle, in render, every change runs render
+    // http request to get songs
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        this.setState({
+            galleryList: response.data
+        })
+      })
+      .catch((error) => {
+        console.log('error on GET', error);
+      })
+  }
   render() {
     return (
       <div className="App">
@@ -9,8 +35,17 @@ class App extends Component {
           <h1 className="App-title">Gallery of my life</h1>
         </header>
         <br/>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+      
+  
+        <div>{this.state.galleryList.map(item => (
+          <>
+          
+          <img src={item.path} />
+          <p>{item.description}</p>
+          <button key={item.id}>Love it!</button>
+          <p>Likes: {item.likes}</p>
+          </>
+        ))}</div>
       </div>
     );
   }
