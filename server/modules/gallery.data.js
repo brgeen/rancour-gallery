1d03e8fd2487dd51a32608b441ccb1f1641c3656
Brooks Geenen <brgeen@gmail.com>
2019-10-27 12:49:20 -0500

    add new images and style DIV's and CSS accordingly

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,16 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat.jpg', description: 'Goat in Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/cat.jpg', description: 'This is my cat Tabby.', likes: 3 },
-    { id: 3, path: 'images/chipmunk.jpg', description: 'I found this chipmunk in the park.', likes: 2 },
-    { id: 4, path: 'images/dog.jpg', description: 'My friends dog, Howard', likes: 5 },
-    { id: 5, path: 'images/weasel.jpg', description: 'Weasels are cute looking', likes: 15 }
+    { id: 1, path: 'images/CarmichaelRoadWisconsin.jpg', description: 'Carmichael Road, Wisconsin. 2018', likes: 0 },
+    { id: 2, path: 'images/DeforestWisconsin.jpg', description: 'This is my cat Tabby.', likes: 3 },
+    { id: 3, path: 'images/ElmwoodWisconsin.jpg', description: 'I found this chipmunk in the park.', likes: 2 },
+    { id: 4, path: 'images/GlacierViewDriveWisconsin.jpg', description: 'My friends dog, Howard', likes: 5 },
+    { id: 5, path: 'images/HickoryLaneWisconsin.jpg', description: 'Weasels are cute looking', likes: 34 },
+    { id: 6, path: 'images/HudsonWisconsin.jpg', description: 'Weasels are cute looking', likes: 56 },
+    { id: 7, path: 'images/ParklandVillageMinnesota.jpg', description: 'Weasels are cute looking', likes: 23 },
+    { id: 8, path: 'images/QuakingBogMinnesota.jpg', description: 'Weasels are cute looking', likes: 67 },
+    { id: 9, path: 'images/SpringValleyWisconsin.jpg', description: 'Weasels are cute looking', likes: 12 },
+    { id: 10, path: 'images/CanonCityMinnesota.jpg', description: 'Weasels are cute looking', likes: 5 },
+    { id: 11, path: 'images/ViennaWisconsin.jpg', description: 'Weasels are cute looking', likes: 78 },
+    { id: 12, path: 'images/ViolaKansas.jpg', description: 'Weasels are cute looking', likes: 90 },
 ];
 
 module.exports = galleryItems;
